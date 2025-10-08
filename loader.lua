07d72de09689dc2431831b20448fcff5aefcddc9
KlarHub <delansouth@yahoo.com>
2025-10-07 21:54:07 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 8204899140 or game.PlaceId == 8206123457 or game.PlaceId == 17541256525 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c07b4a952d231796c162e4f594776472.lua"))()
+elseif table.find({99588440661442, 9622750053}, game.PlaceId) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dee331cdc38ff24e05d1c504bbbf256e.lua"))()
 elseif game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9f33cb120d11ef400205b4518c0ee83e.lua"))()
 elseif game.PlaceId == 12355337193 then
