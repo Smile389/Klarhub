eb8b2fa06d34e22c6eb9352f1c68eb24a55b2a20
KlarHub <delansouth@yahoo.com>
2025-10-10 20:52:15 -0400

    Create Free Loader.lua

---
+++ Free Loader.lua
@@ -0,0 +1,5 @@
+if table.find({99588440661442, 9622750053}, game.PlaceId) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9f69e63adbab099b27d9b7217ba10570.lua"))()
+elseif table.find({4940687511, 13834702475, 14801724413, 4822225642, 13415948659, 2338325648}, game.PlaceId) then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/50da22b3657a22c353b0dde631cb1dcf.lua"))()
+end
