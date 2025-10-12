21fcab8cd11c3e29005aa197aab62113e28c3472
KlarHub <delansouth@yahoo.com>
2025-10-12 10:18:34 -0400

    Update Free Loader.lua

--- Free Loader.lua
+++ Free Loader.lua
@@ -1,5 +1,17 @@
-if table.find({99588440661442, 9622750053}, game.PlaceId) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9f69e63adbab099b27d9b7217ba10570.lua"))()
-elseif table.find({8204899140, 17541256525, 14801724413, 8206123457}, game.PlaceId) then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/50da22b3657a22c353b0dde631cb1dcf.lua"))()
+local PlaceIds = {
+    -- // Flag FB
+    [99588440661442] = "https://api.luarmor.net/files/v3/loaders/9f69e63adbab099b27d9b7217ba10570.lua",
+    [9622750053] = "https://api.luarmor.net/files/v3/loaders/9f69e63adbab099b27d9b7217ba10570.lua",
+    
+    -- // FF2
+    [8204899140] = "https://api.luarmor.net/files/v3/loaders/50da22b3657a22c353b0dde631cb1dcf.lua",
+    [8206123457] = "https://api.luarmor.net/files/v3/loaders/50da22b3657a22c353b0dde631cb1dcf.lua",
+    [17541256525] = "https://api.luarmor.net/files/v3/loaders/50da22b3657a22c353b0dde631cb1dcf.lua"
+}
+
+local currentPlaceId = game.PlaceId
+if PlaceIds[currentPlaceId] then
+    loadstring(game:HttpGet(PlaceIds[currentPlaceId]))()
+else
+    game.Players.LocalPlayer:Kick("Wrong Game Check Supported Games (Free)")
 end
