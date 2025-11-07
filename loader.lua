62659e0b760d9f893f180199e56eebc86f3b3671
KlarHub <delansouth@yahoo.com>
2025-11-07 15:14:36 -0500

    Add new PlaceId to loader condition

--- loader.lua
+++ loader.lua
@@ -8,6 +8,6 @@ elseif game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9f33cb120d11ef400205b4518c0ee83e.lua"))()
 elseif game.PlaceId == 12355337193 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cd9c2bc2308e8b38e528af2cfe332e86.lua"))()
-elseif table.find({4940687511, 13834702475, 14801724413, 4822225642, 13415948659, 2338325648}, game.PlaceId) then
+elseif table.find({4940687511, 13834702475, 14801724413, 4822225642, 13415948659, 2338325648, 129992427163618}, game.PlaceId) then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/defefac959a1472f59767c578c111178.lua"))()
 end
